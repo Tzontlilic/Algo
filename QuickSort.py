792e13424efbb7177c9cf96e2b5c2f2f104f671d
tzontlilic <767406972@qq.com>
2019-05-11 01:04:25 +0800

    QucikSort

---
+++ QuickSort.py
@@ -0,0 +1,12 @@
+def QuickSort(lists):
+	key = lists[0]
+
+	while(i != j):
+
+		for j in range(len(lists) - 1, i):
+			if lists[j] < key:
+				swap(lists[i], lists[j])
+				break
+			else:
+				continue
+
