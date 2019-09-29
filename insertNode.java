2fe0cc92b1baf87d12e108bf42fe12e2180dc286
Tzontlilic <767406972@qq.com>
2019-09-29 14:11:49 +0800

    add delete func

--- insertNode.java
+++ insertNode.java
@@ -13,11 +13,11 @@ public class insertNode {
 		mNode.insert(3, 3);
 		mNode.insert(0, -1);
 		mNode.insert(2, 15);
-		System.out.println("size:" + mNode.size);
-		//mNode.insert(0, -1);
 		mNode.print();
-
-
+		mNode.deleteNode(0);
+		//mNode.insert(0, -1);
+		System.out.println("-----");
+		mNode.print();
 	}
 
 	public void print(){
@@ -31,10 +31,29 @@ public class insertNode {
 	public Node get(int index) throws Exception{
 		Node target;
 		target = head;
-		for(int i = 0; i <= index; i ++) {
-			target = head.next;
+		for(int i = 0; i < index; i ++) {
+			target = target.next;
 		}		
 		return target;
+	}
+
+	public void deleteNode(int index) throws Exception{
+		
+		if(index < 0 || index > size){
+			throw new IndexOutOfBoundsException("索引超出范围");
+		}
+		Node deleteNode = get(index);
+		if(index == 0){// 如果是删除链表头
+			head = deleteNode.next;
+		}else if(index == size){ // 如果删除的是表尾
+			Node preNode = get(index - 1); // 获取上一个节点		
+			last = preNode;
+		} else { // 如果是在表中
+			Node preNode = get(index - 1);
+			preNode.next = deleteNode.next;	
+		}
+		deleteNode = null;
+		size--;
 	}
 
 	public void insert(int index, int data) throws Exception{
