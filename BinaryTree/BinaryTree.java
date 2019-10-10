78b7336f68caa7be24d7d118dc18db14ebc413c9
Tzontlilic <767406972@qq.com>
2019-10-10 13:59:48 +0800

    添加了广度优先遍历

--- BinaryTree/BinaryTree.java
+++ BinaryTree/BinaryTree.java
@@ -1,5 +1,10 @@
+/*
+ * 二叉树的遍历
+ * Author: Tzontlilic
+ */
 import java.util.LinkedList;
 import java.util.Arrays;
+import java.util.Queue;
 
 public class BinaryTree{
 
@@ -24,6 +29,7 @@ public class BinaryTree{
 		return node;
 	}
 	
+	/*                                               深度优先遍历                               */
 	// 二叉树前序遍历
 	// 前序遍历：若一个结点存在左孩子，则输出左孩子，直到没有左孩子，返回上一个结点，输出右孩子
 	// 如果既没有左孩子也没有右孩子，则返回
@@ -67,6 +73,28 @@ public class BinaryTree{
 	}
 	
 
+	/*                                                广度优先遍历                             */
+	// 利用队列的数据结构，实现层级遍历。
+	public static void levelOrderTraversal(TreeNode root){
+	
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		// 向队列插入一个元素
+		queue.offer(root);
+		// 判断队列是否为空，如果为空，则表示遍历完成了， 如果不为空，则继续遍历
+		while(!queue.isEmpty()){
+			// poll():删除队头的一个元素
+			TreeNode node = queue.poll();
+			// 输出队头的这个元素， 表示已经遍历过了
+			System.out.println(node.data);
+			//判断输出的这个元素是否有左右孩子
+			if(node.leftChild != null){
+				queue.offer(node.leftChild);	
+			}
+			if(node.rightChild != null){
+				queue.offer(node.rightChild);
+			}
+		}
+	}
 
 	// 测试用主函数 
 	public static void main(String[] args){
@@ -91,5 +119,7 @@ public class BinaryTree{
 		mBt.inOrderTraveral(treeNode);
 		System.out.println("后序遍历：");
 		mBt.postOrderTraveral(treeNode);
-	
+		System.out.println("广度优先遍历");
+		mBt.levelOrderTraversal(treeNode);
+	}
 }
